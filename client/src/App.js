c0c55e6d247e6cd61b9ad063e544dd0e9cca3d00
abhi62 <abhi.leon.93@gmail.com>
2021-11-09 16:14:18 +0530

    fix: test text change

--- client/src/App.js
+++ client/src/App.js
@@ -39,7 +39,7 @@ const App = () => {
     <div className={classes.wrapper}>
       <AppBar className={classes.appBar} position='static' color='inherit'>
         <Typography variant='h2' align='center'>
-          🐼 Video Chat
+          🐼 Video Chat Fix
         </Typography>
       </AppBar>
       <VideoPlayer />
